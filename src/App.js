f991c03ff1b1648eaf9ad1661eecae6581249f46
soumen974 <sob99338@gmail.com>
2024-12-24 17:44:33 +0530

    local storage for auth added:

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Layout from "./Layout/Layout";
 import SignIn from "./Auth/Signin";
 import Home from "./pages/Home";
 import HowtoUse from "./pages/HowtoUse";
 
 function App() {
-  
+
+  const isAuthenticated = localStorage.getItem("User") === 'true'
+
+
   return (
    <>
    <Router>
     <Routes>
-      <Route path='/' element={<Layout/>}>
-            <Route index element={<Home/>}/>
-            <Route path='/how-to-use' element={<HowtoUse/>}/>
-            <Route path='/settings' element={<HowtoUse/>}/>
-            <Route path='/support' element={<HowtoUse/>}/>
-            <Route path='/aicoder' element={<HowtoUse/>}/>
-            <Route path='/cloudsecurity' element={<HowtoUse/>}/>
-
-      </Route>
-      <Route path='/signin' element={<SignIn/>}/>
+        <Route path="/signin" element={isAuthenticated ? <Navigate to="/" replace /> : <SignIn />} />
+        <Route path="/" element={isAuthenticated ? <Layout /> : <Navigate to="/signin" replace />}>
+          <Route index element={<Home />} />
+          <Route path="how-to-use" element={<HowtoUse />} />
+          <Route path="settings" element={<HowtoUse />} />
+          <Route path="support" element={<HowtoUse />} />
+          <Route path="aicoder" element={<HowtoUse />} />
+          <Route path="cloudsecurity" element={<HowtoUse />} />
+        </Route>
     </Routes>
     </Router>   
    </>
--- src/Auth/Signin.jsx
+++ src/Auth/Signin.jsx
@@ -20,15 +20,20 @@ const Signin = () => {
 
   const authLoginMethods = (methods) =>{
     if(methods === 'github'){
-      window.location.href = `https://www.google.com/search?q=github.com`;
+      // window.location.href = `https://www.google.com/search?q=github.com`;
+      localStorage.setItem("User", 'true');
     }else if(methods === 'bitbucket'){
-      window.location.href = `https://www.google.com/search?q=bitbucket.com`;
+      // window.location.href = `https://www.google.com/search?q=bitbucket.com`;
+      localStorage.setItem("User", 'true');
     }else if(methods === 'Azure'){
-      window.location.href = `https://www.google.com/search?q=azure.com`;
+      // window.location.href = `https://www.google.com/search?q=azure.com`;
+      localStorage.setItem("User", 'true');
     }else if(methods === 'gitlab'){
-      window.location.href = `https://www.google.com/search?q=gitlab.com`;
+      // window.location.href = `https://www.google.com/search?q=gitlab.com`;
+      localStorage.setItem("User", 'true');
     }else if(methods === 'sso'){
-      window.location.href = `https://www.google.com/search?q=sso`;
+      // window.location.href = `https://www.google.com/search?q=sso`;
+      localStorage.setItem("User", 'true');
     }else{
       window.location.href = `/`;
     }
